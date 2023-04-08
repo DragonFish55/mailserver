1d531474a2d7cf5c1cfb000644ba59bca9d853c0
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2023-04-08 16:53:06 -0400

    Updated backend

--- src/backend/db.go
+++ src/backend/db.go
@@ -3,26 +3,24 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/lib/pq"
 )
-
-
 
 func createDbConnect(dbname string) (db *sql.DB, err_out error) {
 	sqlconn := "host=" + dbaddr + " port=" + dbport + " user=" + dbuser + " password=" + dbpass + " dbname=" + dbname + " sslmode=disable"
 	fmt.Println(sqlconn)
-	dbmain, err := sql.Open(dbtype, sqlconn);
-	if(err != nil){
-		fmt.Println("eror");
-	} 
-	err = dbmain.Ping();
-	return dbmain, err;
+	dbmain, err := sql.Open(dbtype, sqlconn)
+	if err != nil {
+		fmt.Println("eror")
+	}
+	err = dbmain.Ping()
+	return dbmain, err
 }
 
-func register_user()
+//func register_user()
 
-func checkUserValid(password string, key string) bool{
+func checkUserValid(password string, key string) bool {
 	return true
 
 }
-
--- src/backend/server.go
+++ src/backend/server.go
@@ -1,112 +1,114 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
+	"log"
+	"net/http"
 )
 
-func create_server(address: string) {
-	
+func create_server(address string) {
+
 	handler := http.NewServeMux()
 	s := &http.Server{
-		Addr:address,
-		Handler: handler
+		Addr:    address,
+		Handler: handler,
 	}
 
-	servers[address] = s
+	servers[address] = *s
 
-	http.HandleFunc("/signup", regUser);
-	http.HandleFunc("/signin", loginUser);
-	http.HandleFunc("/logout", logoutUser);
-	http.HandleFunc("/logout", logout_user);
-	http.HandleFunc("/shutdown", shutdown_server(s));
-	http.HandleFunc("/newserver", create_server());
-	
-	http.ListenAndServe(saddress,nil);
+	http.HandleFunc("/signup", regUser)
+	http.HandleFunc("/signin", loginUser)
+	//http.HandleFunc("/logout", logoutUser)
+	http.HandleFunc("/logout", logout_user)
+	http.HandleFunc("/shutdown", shutdown_server)
+	http.HandleFunc("/newserver", create_server)
+
+	http.ListenAndServe(saddress, nil)
 }
 
-
-
-
-func shutdown_server(w http.ResponseWriter, r *http.Request, server http.Server){
+func shutdown_server(w http.ResponseWriter, r *http.Request, server http.Server) {
 	w.Write([]byte("OK"))
-    go func() {
-        if err := s.Shutdown(context.Background()); err != nil {
-            log.Fatal(err)
-        }
-    }()
+	go func() {
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
 
-func check_server_status(server http.Server) bool {
+//func check_server_status(server http.Server) bool {
 
-}
+//}
 
-func logout_user(w http.ResponseWriter, r *http.Request){
+func logout_user(w http.ResponseWriter, r *http.Request) {
 
 }
 
 //login user account
-func loginUser(w http.ResponseWriter, r *http.Request){
-	
-	userInfo, err := ioutil.ReadAll(r.Body);
-	if err != nil{ fmt.Println("Error add user reading json data"); }
-	var user_reg UserLogin = UserLogin{Username:"", Password:"",};
-	var key string;
-	var pass string;
-	err = json.Unmarshal([]byte(userInfo), &user_reg);
-	if err != nil { 
-		fmt.Println("Error extracting json add user data");
-		w.Write([]byte(`{"valid":"true"}`));
+func loginUser(w http.ResponseWriter, r *http.Request) {
+
+	userInfo, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error add user reading json data")
 	}
-	sqlconn, err := createDbConnect("Users");
-	if err != nil { 
-		fmt.Println("Could not connect to db");
-		w.Write([]byte(`{"valid":"true"}`));
-	};
-	querRows, err := sqlconn.Query("SELECT password, enckey FROM USER WHERE username = ?", user_reg.Username)
-	if err != nil { 
-		fmt.Println("Could not query User table");
-		w.Write([]byte(`{"valid":"true"}`));
-	};
-	
-	defer querRows.Close();
+	var user_reg UserLogin = UserLogin{username: "", password: ""}
+	var key string
+	var pass string
+	err = json.Unmarshal([]byte(userInfo), &user_reg)
+	if err != nil {
+		fmt.Println("Error extracting json add user data")
+		w.Write([]byte(`{"valid":"true"}`))
+	}
+	sqlconn, err := createDbConnect("Users")
+	if err != nil {
+		fmt.Println("Could not connect to db")
+		w.Write([]byte(`{"valid":"true"}`))
+	}
+	querRows, err := sqlconn.Query("SELECT password, enckey FROM USER WHERE username = ?", user_reg.username)
+	if err != nil {
+		fmt.Println("Could not query User table")
+		w.Write([]byte(`{"valid":"true"}`))
+	}
+
+	defer querRows.Close()
 
 	for querRows.Next() {
-        err := querRows.Scan(&pass, &key)
-        if err != nil {
+		err := querRows.Scan(&pass, &key)
+		if err != nil {
 			panic(err)
 		} else {
 			if checkUserValid(pass, key) {
-				fmt.Println("Password is Valid");
+				fmt.Println("Password is Valid")
 				w.Write([]byte(`{"valid":"true"}`))
-				break;
+				break
 			}
-			fmt.Println("Password is Invalid");
+			fmt.Println("Password is Invalid")
 			w.Write([]byte(`{"valid":"false"}`))
-			break;
+			break
 		}
-    }
-}
-
-// Add new user account to email database
-func regUser(w http.ResponseWriter, r *http.Request){
-	var user_reg UserReg = UserReg{
-		Username: "",
-		Password: "",
-		Confpass: "",
-		Fname: "",
-		Lname: "",
-		Bday: "",
-	};
-
-	userInfo, err := ioutil.ReadAll(r.Body);
-	if err != nil{ fmt.Println("Error add user reading json data"); }
-	err = json.Unmarshal(userInfo, &user_reg);
-	if err != nil { 
-		fmt.Println("Error extracting json add user data");
-		w.Write([]byte(`{"valid":"true"}`));
 	}
 }
 
+// Add new user account to email database
+func regUser(w http.ResponseWriter, r *http.Request) {
+	var user_reg UserReg = UserReg{
+		username: "",
+		password: "",
+		confpass: "",
+		fname:    "",
+		lname:    "",
+		bday:     "",
+	}
+
+	userInfo, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error add user reading json data")
+	}
+	err = json.Unmarshal(userInfo, &user_reg)
+	if err != nil {
+		fmt.Println("Error extracting json add user data")
+		w.Write([]byte(`{"valid":"true"}`))
+	}
+}
